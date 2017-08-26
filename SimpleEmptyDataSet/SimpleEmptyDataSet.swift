25255c0e8013e3bd41a8be84fcaf00a6e14ab0d4
Anirudha <anirudhamahale@gmail.com>
2017-08-26 12:34:15 +0530

    first commit

---
+++ SimpleEmptyDataSet/SimpleEmptyDataSet.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleEmptyDataSet.swift
+//  SimpleEmptyDataSet
+//
+//  Created by Anirudha on 26/08/17.
+//  Copyright © 2017 Anirudha Mahale. All rights reserved.
+//
+
+import UIKit
+
+class SimpleEmptyDataSet: UIView {
+    
+    @IBOutlet var view: UIView!
+    
+
+}
---
+++ SimpleEmptyDataSet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleEmptyDataSet
+//
+//  Created by Anirudha on 26/08/17.
+//  Copyright © 2017 Anirudha Mahale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
